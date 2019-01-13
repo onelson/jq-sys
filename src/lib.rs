10645884f8defb68c3a3c56ebd2435ac09482d21
Owen Nelson <onelson@gmail.com>
2019-01-12 20:40:34 -0800

    initial checkin

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+extern crate jq_src;
+
+mod bindings;
+pub use bindings::*;
+
+#[cfg(test)]
+mod tests {
+    use super::{jq_init, jq_state, jq_teardown};
+
+    #[test]
+    fn can_init_and_teardown() {
+        unsafe {
+            let state: *mut jq_state = jq_init();
+            jq_teardown(state as *mut *mut jq_state);
+        }
+    }
+}
